7ca9fdbd084119507509ab89c79500bfa478ebe2
dddddai <dddwq@foxmail.com>
2022-04-07 15:10:54 +0800

    add support for ipv6 (control-plane)

--- pkg/linux/ip.go
+++ pkg/linux/ip.go
@@ -25,12 +25,18 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func IP2Linux(ipstr string) (uint32, error) {
+func IP2Linux(ipstr string) (unsafe.Pointer, error) {
 	ip := net.ParseIP(ipstr)
 	if ip == nil {
-		return 0, fmt.Errorf("error parse ip: %s", ipstr)
+		return nil, fmt.Errorf("error parse ip: %s", ipstr)
 	}
-	return *(*uint32)(unsafe.Pointer(&ip[12])), nil
+	if !strings.ContainsRune(ipstr, ':') {
+		// ipv4, we need to clear the bytes
+		for i := 0; i < 12; i++ {
+			ip[i] = 0
+		}
+	}
+	return unsafe.Pointer(&ip[0]), nil
 }
 
 func IsCurrentNodeIP(ipstr string, ipListFile string) bool {
