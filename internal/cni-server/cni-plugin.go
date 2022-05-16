7d0d0cf5d7ba2a113dc1f8fd9affc9ac58cdb678
Kebe <mail@kebe7jun.com>
2022-05-16 16:57:38 +0800

    + add hardware checksum option for cross node pods communication (#162)
    * > fix panic if get netns error
    
    * + add hardware checksum option for cross node pods communication
    
    * + do on repair
    
    Co-authored-by: Xunzhuo <mixdeers@gmail.com>

--- internal/cni-server/cni-plugin.go
+++ internal/cni-server/cni-plugin.go
@@ -147,6 +147,13 @@ func (s *server) CmdAdd(args *skel.CmdArgs) (err error) {
 	}
 	// attach xdp to the veth pair
 	err = netnsPairEthDo(netNS.Path(), func(name string, index int) error {
+		if !s.hardwareCheckSum {
+			log.Debugf("disable hardware checksum for pod(%s/%s)", string(k8sArgs.K8S_POD_NAMESPACE), string(k8sArgs.K8S_POD_NAME))
+			c := exec.Command("sh", "-c", fmt.Sprintf("ethtool -K %s tx off", name))
+			if err := c.Run(); err != nil {
+				return fmt.Errorf("disable %s tx off error: %v", name, err)
+			}
+		}
 		xdp, err := ebpf.LoadPinnedProgram(path.Join(s.bpfMountPath, "mb_xdp"), &ebpf.LoadPinOptions{})
 		// todo support load by ID: xdp, err := ebpf.NewProgramFromID(1595)
 		if err != nil {
@@ -291,6 +298,13 @@ func (s *server) checkAndRepairPodPrograms() error {
 			}
 			// attach xdp to the veth pair
 			if err = netnsPairEthDo(netns.Path(), func(name string, index int) error {
+				if !s.hardwareCheckSum {
+					log.Debugf("disable hardware checksum for pid(%s)", pid)
+					c := exec.Command("sh", "-c", fmt.Sprintf("ethtool -K %s tx off", name))
+					if err := c.Run(); err != nil {
+						return fmt.Errorf("disable %s tx off error: %v", name, err)
+					}
+				}
 				xdp, err := ebpf.LoadPinnedProgram(path.Join(s.bpfMountPath, "mb_xdp"), &ebpf.LoadPinOptions{})
 				// todo support load by ID: xdp, err := ebpf.NewProgramFromID(1595)
 				if err != nil {
