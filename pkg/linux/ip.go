e6a92d3163fc1f2273b1641d2bda739dc202518d
dddddai <dddwq@foxmail.com>
2022-07-15 22:05:47 +0800

    use host name to filter pods

--- pkg/linux/ip.go
+++ pkg/linux/ip.go
@@ -19,11 +19,7 @@ package linux
 import (
 	"fmt"
 	"net"
-	"os"
-	"strings"
 	"unsafe"
-
-	log "github.com/sirupsen/logrus"
 )
 
 func IP2Linux(ipstr string) (uint32, error) {
@@ -32,40 +28,4 @@ func IP2Linux(ipstr string) (uint32, error) {
 		return 0, fmt.Errorf("error parse ip: %s", ipstr)
 	}
 	return *(*uint32)(unsafe.Pointer(&ip[12])), nil
-}
-
-func IsCurrentNodeIP(ipstr string, ipListFile string) bool {
-	if ipListFile != "" {
-		// exists
-		bs, err := os.ReadFile(ipListFile)
-		if err != nil {
-			log.Errorf("read ip list file from %s error: %v", ipListFile, err)
-		} else {
-			for _, line := range strings.Split(string(bs), "\n") {
-				if strings.HasPrefix(line, ipstr+"/") {
-					return true
-				}
-			}
-			return false
-		}
-	}
-	log.Debugf("no ips file found, fetch ips from interfaces")
-	ifaces, _ := net.Interfaces() // todo: handle err
-	for _, i := range ifaces {
-		addrs, _ := i.Addrs() // todo: handle err
-		for _, addr := range addrs {
-			switch v := addr.(type) {
-			case *net.IPNet:
-				if v.IP.String() == ipstr {
-					return true
-				}
-			case *net.IPAddr:
-				if v.String() == ipstr {
-					return true
-				}
-			}
-			// todo: process IP address
-		}
-	}
-	return false
 }
