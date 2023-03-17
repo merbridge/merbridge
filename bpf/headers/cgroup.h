723437252f9ac7b8ffea130ee7e5d08541f6a87f
Cybwan <baili@flomesh.io>
2023-03-17 15:24:16 +0800

    remove unreachable code.
    signed-off-by: cybwan <baili@flomesh.io>

--- bpf/headers/cgroup.h
+++ bpf/headers/cgroup.h
@@ -54,6 +54,7 @@ static inline int get_current_cgroup_info(void *ctx,
         if (s) {
             __u32 curr_ip_mark = s->mark;
             bpf_sk_release(s);
+            // get ip addresses of current pod/ns.
             __u32 *ip =
                 (__u32 *)bpf_map_lookup_elem(&mark_pod_ips_map, &curr_ip_mark);
             if (!ip) {
@@ -75,23 +76,6 @@ static inline int get_current_cgroup_info(void *ctx,
             debugf("can not get port listen for cgroup(%ld)", cgroup_id);
         } else {
             _default.is_in_mesh = 1;
-            // get ip addresses of current pod/ns.
-            struct bpf_sock_tuple tuple = {};
-            tuple.ipv4.dport = bpf_htons(SOCK_IP_MARK_PORT);
-            tuple.ipv4.daddr = 0;
-            struct bpf_sock *s = bpf_sk_lookup_tcp(
-                ctx, &tuple, sizeof(tuple.ipv4), BPF_F_CURRENT_NETNS, 0);
-            if (s) {
-                __u32 curr_ip_mark = s->mark;
-                bpf_sk_release(s);
-                __u32 *ip = (__u32 *)bpf_map_lookup_elem(&mark_pod_ips_map,
-                                                         &curr_ip_mark);
-                if (!ip) {
-                    debugf("get ip for mark 0x%x error", curr_ip_mark);
-                } else {
-                    set_ipv6(_default.cgroup_ip, ip); // network order
-                }
-            }
         }
 #endif
         if (bpf_map_update_elem(&cgroup_info_map, &cgroup_id, &_default,
