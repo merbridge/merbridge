e61e1e682d5543c3ee6959f1fce0948a1085b66e
Kebe <mail@kebe7jun.com>
2023-08-13 17:09:53 +0800

    > fix review
    Signed-off-by: Kebe <mail@kebe7jun.com>

--- internal/process/manager.go
+++ internal/process/manager.go
@@ -1,3 +1,19 @@
+/*
+Copyright © 2022 Merbridge Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package process
 
 import (
@@ -24,9 +40,9 @@ import (
 )
 
 const (
-	AMBIENT_MESH_MODE_FLAG = (1 << 0)
-	ZTUNNEL_FLAG           = (1 << 2)
-	ZTUNNEL_IP_KEY         = 0x1
+	AMBIENT_MESH_MODE_FLAG = (1 << 0) // nolint: stylecheck
+	ZTUNNEL_FLAG           = (1 << 2) // nolint: stylecheck
+	ZTUNNEL_IP_KEY         = 0x1      // nolint: stylecheck
 )
 
 type cgroupInfo struct {
@@ -49,7 +65,7 @@ type cgroupInfo struct {
 	*/
 	ID            uint64
 	IsInMesh      uint32
-	CgroupIp      [4]uint32
+	CgroupIP      [4]uint32
 	Flags         uint16
 	DetectedFlags uint16
 }
@@ -206,7 +222,7 @@ func (w *processManager) writePodInfoToCgroupMap(cgroupInode uint64, pod podInfo
 	tcg := cgroupInfo{
 		ID:            cgroupInode,
 		IsInMesh:      in,
-		CgroupIp:      *(*[4]uint32)(_ip),
+		CgroupIP:      *(*[4]uint32)(_ip),
 		Flags:         flag,
 		DetectedFlags: cgrinfo.DetectedFlags | AMBIENT_MESH_MODE_FLAG | ZTUNNEL_FLAG,
 	}
